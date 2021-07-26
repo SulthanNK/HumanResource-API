386bb2a2f1e19d09f1d763f05b051719a72967b4
Sulthan Mohaideen <sulthanmohaideen1998@gmail.com>
2021-07-26 16:18:04 +0530

    Department class completed

---
+++ src/humanresource_api/Department.java
@@ -0,0 +1,70 @@
+package humanresource_api;
+
+public class Department {
+    private String name;
+
+    private Employee[] employees = new Employee[10];
+
+    private int lastAddedEmployeeIndex = -1;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public void addEmployee(Employee anEmployee) {
+        if (lastAddedEmployeeIndex < employees.length) {
+            lastAddedEmployeeIndex++;
+            employees[lastAddedEmployeeIndex] = anEmployee;
+        }
+    }
+
+    public Employee[] getEmployees() {
+        Employee[] actualEmployees = new Employee[lastAddedEmployeeIndex + 1];
+
+        for (int i = 0; i < actualEmployees.length; i++) {
+            actualEmployees[i] = employees[i];
+        }
+
+        return actualEmployees;
+    }
+
+    public int getEmployeeCount() {
+        return lastAddedEmployeeIndex + 1;
+    }
+
+    public Employee getEmployeeID(int empId) {
+        for (Employee emp : employees) {
+            if (emp.getID() == empId) {
+                return emp;
+            }
+        }
+        return null;
+    }
+
+    public double getTotalSalary() {
+        double totalSalary = 0.0;
+        for (int i = 0; i <= lastAddedEmployeeIndex; i++) {
+            totalSalary += employees[i].getSalary();
+        }
+        return totalSalary;
+    }
+
+    public double getAverageSalary() {
+        if (lastAddedEmployeeIndex > -1) {
+            return getTotalSalary() / (lastAddedEmployeeIndex + 1);
+        }
+        return 0.0;
+    }
+}
--- src/humanresource_api/HRApp.java
+++ src/humanresource_api/HRApp.java
@@ -4,9 +4,22 @@ public class HRApp {
     public static void main(String[] args) {
         System.out.println("API starts running...");
 
-        Employee EmployeeOne = new Employee(574839,"John Henry", 523378.36);
-        Employee EmployeeTwo = new Employee(574632,"Andrew Mark", 534378.36);
-        System.out.println(EmployeeOne);
-        System.out.println(EmployeeTwo);
+        Employee EmployeeOne = new Employee(574839, "John Henry", 523378.36);
+        Employee EmployeeTwo = new Employee(574632, "Andrew Mark", 534378.36);
+
+        Department eduDept = new Department("Education");
+
+        eduDept.addEmployee(EmployeeOne);
+        eduDept.addEmployee(EmployeeTwo);
+        eduDept.addEmployee(new Employee(574923, "Elizabeth Fillips", 497856.67));
+
+        Employee[] employeeArray = eduDept.getEmployees();
+
+        for (Employee employee : employeeArray) {
+            System.out.println(employee);
+        }
+        System.out.println("\nTotal Salary: " + eduDept.getTotalSalary());
+        System.out.println("Average Salary: " + eduDept.getAverageSalary());
+        System.out.println("Random Employee: \n" + eduDept.getEmployeeID(574632));
     }
 }
